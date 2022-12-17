335c4c7786764b2eca3f57c73e6202d8c96e829a
panchenko-kate <kmaslova_007@ukr.net>
2022-12-17 22:18:41 +0200

    +

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,10 +1,9 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
 const imgContainer = document.querySelector('.gallery');
 const imgsCardsMarkup = createImageCardsMurkup(galleryItems);
 
-imgContainer.insertAdjacentHTML('beforebegin', imgsCardsMarkup);
+imgContainer.insertAdjacentHTML('beforeend', imgsCardsMarkup);
 imgContainer.addEventListener('click', onImgContainerClick);
 
 function createImageCardsMurkup (imgs) {
@@ -26,3 +25,24 @@ function createImageCardsMurkup (imgs) {
 ;
 };
 
+function onImgContainerClick(evt) {
+    evt.stopPropagation();
+    evt.preventDefault();
+
+    const isImgCardEl = evt.target.classList.contains('gallery__image');
+
+    if(!isImgCardEl) {
+        return;
+    }
+
+    const imgEl = evt.target;
+    const originalImgSizeRef = imgEl.dataset.source;
+    console.log(originalImgSizeRef);
+
+    const instance = basicLightbox.create(`
+    <img
+      src="${originalImgSizeRef}"
+    />
+    `)
+    instance.show();
+};
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,48 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const imgContainer = document.querySelector('.gallery');
+const imgsCardsMarkup = createImageCardsMurkup(galleryItems);
 
-console.log(galleryItems);
+imgContainer.insertAdjacentHTML('beforeend', imgsCardsMarkup);
+// imgContainer.addEventListener('click', onImgContainerClick);
+
+function createImageCardsMurkup (imgs) {
+    return imgs.map(({preview, original, description}) => {
+        return `
+        <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+        `;
+    })
+    .join('')
+;
+};
+
+var gallery = new SimpleLightbox('.gallery a', {captionsData: 'alt', captionDelay: 250,});
+gallery.on('shown.simplelightbox', function () {
+	// do something…
+    
+});
+
+// function onImgContainerClick(evt) {
+//     evt.stopPropagation();
+//     evt.preventDefault();
+
+//     const isImgCardEl = evt.target.classList.contains('gallery__image');
+
+//     if(!isImgCardEl) {
+//         return;
+//     }
+
+//     const imgEl = evt.target;
+//     const originalImgSizeRef = imgEl.dataset.source;
+//     console.log(originalImgSizeRef);
+
+//     const instance = basicLightbox.create(`
+//     <img
+//       src="${originalImgSizeRef}"
+//     />
+//     `)
+//     instance.show();
+// };
+// console.log(galleryItems);
